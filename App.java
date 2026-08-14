5f7b896849594bee620f53947853f35685a07ebf
Alby <albygeorgeshiju2004@gmail.com>
2026-08-14 11:05:47 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,40 +1,40 @@
+package com.example;
+
 public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 
     public static void main(String[] args) {
 
-        // Hardcoded password - SonarQube should flag this
-        String password = "Admin@123";
-
-        // Unused variable
-        String username = "admin";
-
-        int a = 10;
-        int b = 0;
-
-        // Potential division by zero
-        int result = a / b;
+        int result = add(10, 20);
 
         System.out.println("Result: " + result);
 
-        // Duplicate code
-        printMessage();
-        printMessage();
+        String unusedVariable = "SonarQube";
 
-        // Empty catch block
-        try {
-            String text = null;
-            System.out.println(text.length());
-        } catch (Exception e) {
+        if (result == 30) {
+            System.out.println("Correct result");
         }
-    }
 
-    public static void printMessage() {
-        System.out.println("Hello World");
-        System.out.println("This is a test application");
-    }
+        if (true) {
+            System.out.println("This condition is always true");
+        }
 
-    // Never-used function
-    public static void neverUsedFunction() {
-        System.out.println("This function is never called.");
+        int x = 10;
+        int y = 20;
+
+        if (x < y) {
+            System.out.println("x is smaller");
+        } else {
+            System.out.println("This will never execute");
+        }
+
+        divide(10, 0);
     }
 }
